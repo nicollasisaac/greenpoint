51dbc45734ec290febbe088f4cf4092dc09db257
Nicollas Isaac <79726241+nicollasisaac@users.noreply.github.com>
2022-10-03 09:03:34 -0300

    att questionpage

---
+++ QandA/answer.js
@@ -0,0 +1,10 @@
+const Paianswer = document.querySelector('.Answer');
+var btn = document.querySelector('.TextQandA');
+
+function Answer(numberAnswer){
+    if(answer.style.display === 'block') {
+        Paianswer.append("O greenPoint is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.");
+    } else {
+        answer.style.display = 'block';
+    }
+}
